7acb278967cb6aa185db99ee5bf1bd1b9b696879
Aleksandre Phatsatsia <aleksandrephatsatsia@Aleksandres-iMac.local>
2020-06-07 18:22:07 +0400

    tried

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import express from 'express'
 const gateway = new ApolloGateway({
   serviceList: [
     { name: 'prisma', url: 'http://techinformiPrismaApi:4001/graphql' },
-    // { name: 'researchProjects', url: 'http://localhost:4002/graphql' },
+    {
+      name: 'researchProjects',
+      url: 'http://techinformiResearchProjectsApi:4002/graphql',
+    },
     // { name: 'pageContents', url: 'http://localhost:4003/graphql' }
   ],
 })
